e36a12730225496008a84483c9611d800beca2e7
afahmi <home.afahmi@gmail.com>
2024-10-12 17:01:29 +0700

    init project news template vuejs

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup="setup"></script>
+
+<template>
+    <div id="site">
+        <header id="site-header">
+            <div class="navigation-wrapper">
+                <div>Header Area</div>
+            </div>
+        </header>
+        <main id="site-main"></main>
+        <footer id="site-footer">
+            <div class="footer-wrapper">
+                <div>Footer Area</div>
+            </div>
+        </footer>
+    </div>
+</template>
+
+<style scoped="scoped"></style>
