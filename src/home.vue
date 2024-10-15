ea0459bec3ace5c92227667a7723b934e261ee8b
GilangSatriaTekkom <tekkomgilang26@gmail.com>
2024-10-15 16:00:20 +0700

    15/10/2024 [create single page news desktop + mobile

--- src/home.vue
+++ src/home.vue
@@ -1,21 +1,30 @@
-<script setup="setup"></script>
+<script setup="setup">
+const { createApp, ref } = Vue;
+
+createApp({
+  setup() {
+    const message = ref("Hello vue!");
+    return {
+      message,
+    };
+  },
+}).mount("#app");
+</script>
 
 <template>
-    <div id="site">
-        <header id="site-header">
-            <div class="navigation-wrapper">
-                <div>Header Area</div>
-            </div>
-        </header>
-        <main id="site-main">
-            ini homepage
-        </main>
-        <footer id="site-footer">
-            <div class="footer-wrapper">
-                <div>Footer Area</div>
-            </div>
-        </footer>
-    </div>
+  <div id="site">
+    <header id="site-header">
+      <div class="navigation-wrapper">
+        <div>Header Area</div>
+      </div>
+    </header>
+    <main id="site-main">ini homepage</main>
+    <footer id="site-footer">
+      <div class="footer-wrapper">
+        <div>Footer Area</div>
+      </div>
+    </footer>
+  </div>
 </template>
 
 <style scoped="scoped"></style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,45 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      body: ["Darker Grotesque", "sans-serif"],
+      inter: ["Inter", "sans-serif"],
+      default: ["Darker Grotesque", "sans-serif"],
+      poppins: ["Poppins", "sans-serif"],
+      darkerGrotesque: ["Darker Grotesque", "sans-serif"],
+      roboto: ["Roboto", "sans-serif"],
+      PlusJakartaSans: ["Plus Jakarta Sans", "Poppins", "sans-serif"],
+      firaCode: ["Fira Code", "monospace"],
+      montserrat: ["Montserrat", "sans-serif"],
+      Spartan: ["SpartanMB", "sans-serif"],
+      Bricolage: ["Bricolage", "sans-serif"],
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "25px",
+        md: "80px",
+        lg: "80px",
+        lg2: "80px",
+        xl: "120px",
+      },
+    },
+    screens: {
+      sm: "375px",
+      md: "768px",
+      lg: "1024px",
+      lg2: "1365px",
+      xl: "1440px",
+      "3xl": "1920px",
+    },
+    extend: {
+      colors: {
+        primary: "#0EC5D7",
+        secondary: "#FEC33C",
+        grey: "#868686",
+        dark: "#001F22",
+      },
+    },
   },
   plugins: [],
-}
-
+};
