7acb2285b6a70f38cbcf17eade46c439fdf508e7
satriajun <satria.junmulyana_ti21@nusaputra.ac.id>
2024-10-15 16:14:47 +0700

    vue berita

---
+++ src/HotnewsPage.vue
@@ -0,0 +1,32 @@
+<script>
+import Hotnews from "./component/Hotnews.vue";
+import Lastest from "./component/Lastest.vue";
+
+export default {
+  components: {
+    Hotnews: Hotnews,
+    Lastest: Lastest,
+  }
+}
+</script>
+
+<template>
+  <div id="site">
+    <header id="site-header">
+      <div class="navigation-wrapper">
+        <div>Header Area</div>
+      </div>
+    </header>
+    <main id="site-main">
+      <Hotnews />
+      <Lastest />
+    </main>
+    <footer id="site-footer">
+      <div class="footer-wrapper">
+        <div>Footer Area</div>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/component/Hotnews.vue
@@ -0,0 +1,86 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    <section class="container lg:max-w-[1200px] mx-auto">
+        <div class="pt[71px]">
+            <div class="text-center flex gap-1 flex-wrap justify-center items-center">
+                <div class=" h-0 border-[2px] w-[491px] sm:w-[244px] border-black mt-4"></div>
+                <p class="text-[32px] md:text-[40px] font-semibold text-black">Hot</p>
+                <p class="text-[32px] md:text-[40px] font-semibold text-red-600">News</p>
+                <div class="h-0 border-[2px] w-[491px] sm:w-[244px] border-black mt-4"></div>
+            </div>
+        </div>
+        <div class=" grid grid-cols-1 lg:grid-cols-2 gap-4 pt-[33px]">
+            <!-- Large image on the left -->
+            <div class="relative">
+                <img src="/images/speed.jpeg" alt="Main News" class="w-[590px] h-[588px] object-cover">
+                <div class="absolute p-4 bottom-0">
+                    <p class="md:text-[30px] md:leading-[33.9px] text-white">Record Breaking, IShowSpeed Reaches 1
+                        Million Steaming Views in
+                        Jakarta, Indonesia!</p>
+                    <div class="flex items-start justify-between">
+                        <div class="border-l-4 border-yellow-500 pl-2">
+                            <p class="font-medium text-white text-sm sm:text-base font-poppins">Populer</p>
+                        </div>
+                        <p class="text-sm sm:text-base text-white font-poppins leading-6 ml-4">Sep.24.2024</p>
+                    </div>
+                </div>
+            </div>
+            <!-- Second image -->
+            <div class="flex flex-wrap">
+                <div class="relative">
+                    <img src="/images/puan.jpeg" alt="News 2" class="w-[590px] h-[290px] object-cover">
+                    <div class="absolute p-4 bottom-[10px]">
+                        <p class="md:text-[24px] md:leading-[27.12px] font-bold text-white ">
+                            Record Breaking, IShowSpeed Reaches 1 Million Steaming Views
+                            in Jakarta, Indonesia!</p>
+                        <div class="flex items-start justify-between">
+                            <div class="border-l-4 border-yellow-500 pl-2">
+                                <p class="font-medium text-white text-sm sm:text-base font-poppins">Populer</p>
+                            </div>
+                            <p class="text-sm sm:text-base text-white font-poppins leading-6 ml-4">Sep.24.2024</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="flex justify-between gap-4 pt-[20px]">
+                    <div class="relative w-[50%]">
+                        <img src="/images/komeng1.jpeg" alt="News 3" class="w-[285px] h-[278px] object-cover">
+                        <div class="absolute p-4 bottom-[10px]">
+                            <p class="md:text-[16px] md:leading-[18.08px] font-bold text-white">Rice
+                                Prices in Indonesia are High but Farmers' Income is Low,
+                                What's the Cause?</p>
+                            <div class="flex items-start justify-between">
+                            <div class="border-l-4 border-yellow-500 pl-2">
+                                <p class="font-medium text-white text-sm sm:text-base font-poppins">Populer</p>
+                            </div>
+                            <p class="text-sm sm:text-base text-white font-poppins leading-6 ml-4">Sep.24.2024</p>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="relative w-[50%]">
+                        <img src="/images/emas.png" alt="News 4" class="w-[285px] h-[278px] object-cover">
+                        <div class="absolute p-4 bottom-[10px]">
+                            <p class="md:text-[16px] md:leading-[18.08px] font-bold text-white">
+                                Record Breaking, IShowSpeed Reaches 1 Million Steaming Views
+                                in Jakarta, Indonesia!</p>
+                            <div class="flex items-start justify-between">
+                            <div class="border-l-4 border-yellow-500 pl-2">
+                                <p class="font-medium text-white text-sm sm:text-base font-poppins">Populer</p>
+                            </div>
+                            <p class="text-sm sm:text-base text-white font-poppins leading-6 ml-4">Sep.24.2024</p>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+</template>
+
+<style scoped="scoped"></style>
