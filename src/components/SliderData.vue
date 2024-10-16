2a44c9d204acb75ac03f5c7f8210e5efc7a22bcf
unknown <andikaoktasandira1710@gmail.comgit config --global user.name project-Magang1>
2024-10-16 16:08:52 +0700

    Membuat Search data Berita

---
+++ src/components/SliderData.vue
@@ -0,0 +1,31 @@
+<!-- <template>
+    <div class="slides">
+       <div class="slides-inner">
+            <div v-for="slide in slides">
+                    {{ slides.src }}
+            </div>
+
+       </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            slides: [
+               { src : 'https://picsum.photos/id/237/200/300' },  
+               { src : 'https://picsum.photos/id/237/200/300' },  
+               { src : 'https://picsum.photos/id/237/200/300' },  
+               { src : 'https://picsum.photos/id/237/200/300' },  
+               { src : 'https://picsum.photos/id/237/200/300' }  
+            ]
+            }
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style> -->
