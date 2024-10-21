21d9ab9a8aab43b25ab1eaccbfa4339d5fff0027
satriajun <satria.junmulyana_ti21@nusaputra.ac.id>
2024-10-21 13:57:11 +0700

    home page

--- src/component/Lastest.vue
+++ src/component/Lastest.vue
@@ -69,38 +69,66 @@ export default {
         <div class="container lg:max-w-[1200px] mx-auto px-[24px] md:px-0">
             <div class="grid grid-cols-2 md:grid-cols-2 ">
                 <div class="flex gap-1 flex-wrap">
-                    <h1 class="text-[18px] leading-[21.6px] md:text-[40px] md:leading-[48px] font-semibold font-bricolage text-black">Lastest</h1>
-                    <h1 class="text-[18px] leading-[21.6px] md:text-[40px] md:leading-[48px] font-semibold font-bricolage text-red-600">News</h1>
+                    <h1
+                        class="text-[18px] leading-[21.6px] md:text-[40px] md:leading-[48px] font-semibold font-bricolage text-black">
+                        Lastest</h1>
+                    <h1
+                        class="text-[18px] leading-[21.6px] md:text-[40px] md:leading-[48px] font-semibold font-bricolage text-red-600">
+                        News</h1>
                 </div>
                 <div class="flex justify-end items-center">
                     <div class="flex items-center justify-center">
-                        <p class="text-black text-[12] leading-[14.4px] md:text-[18px] md:leading-[21.6px] font-bricolage pr-[8px]">See All</p>
-                        <img class="w-[14px] h-[9.44px] md:w-[27.7px] md:h-[16.51px]" src="/svg/panah-kanan.svg" alt="Arrow Icon" />
-                </div>
-            </div>
-        </div>
-        
-        <!-- Divider -->
-        <div class="flex gap-[12px] flex-wrap">
-            <div class="border-[2px] w-[81px] md:w-[244px] border-black mt-2"></div>
-            <div class="border-[2px] w-[26px] md:w-[32px] border-black mt-2"></div>
-            <div class="border-[2px] w-[26px] md:w-[32px] border-black mt-2"></div>
-        </div>
-        
-        <!-- Cards -->
-        <div class="grid grid-cols-2 md:grid-cols-4 gap-6 mt-[20px] ">
-            <div v-for="card in cards" :key="card.title" class="">
-                <img :src="card.image" alt="image" class="w-[full] h-[auto] md:w-[285px] md:h-[291px]" />
-                <div class="flex justify-between mt-[14px]">
-                    <div class="border-l-4 border-black">
-                        <p class="ml-[10px] text-[14px] leading-[14px] md:text-[16px] md:leading-[16px] font-bold font-bricolage text-[#002E40]">{{ card.author }}</p>
+                        <p
+                            class="text-black text-[12] leading-[14.4px] md:text-[18px] md:leading-[21.6px] font-bricolage pr-[8px]">
+                            See All</p>
+                        <img class="w-[14px] h-[9.44px] md:w-[27.7px] md:h-[16.51px]" src="/svg/panah-kanan.svg"
+                            alt="Arrow Icon" />
                     </div>
-                    <p class="text-[12px] leading-[12px]  text-[#002E40] font-bricolage">{{ card.date }}</p>
                 </div>
-                <p class="text-[12px] leading-[15.6px] md:text-[20px] md:leading-[26px] font-semibold text-[#1D1717] mt-[12px]">{{ card.title }}</p>
             </div>
+
+            <!-- Divider -->
+            <div class="flex gap-[12px] flex-wrap">
+                <div class="border-[2px] w-[81px] md:w-[244px] border-black mt-2"></div>
+                <div class="border-[2px] w-[26px] md:w-[32px] border-black mt-2"></div>
+                <div class="border-[2px] w-[26px] md:w-[32px] border-black mt-2"></div>
+            </div>
+
+            <!-- Cards -->
+            <div class="grid grid-cols-2 md:grid-cols-4 gap-6 mt-5">
+                <div v-for="card in cards" :key="card.title" class="flex flex-col">
+                    <img :src="card.image" alt="image" class="w-full h-auto md:w-[285px] md:h-[291px]" />
+
+                    <div class="hidden md:block mt-3">
+                        <div class="flex justify-between">
+                            <div class="border-l-4 border-black">
+                                <p class="ml-2 text-[14px] md:text-[16px] font-bold text-[#002E40]">{{ card.author }}
+                                </p>
+                            </div>
+                            <p class="text-[12px] text-[#002E40]">{{ card.date }}</p>
+                        </div>
+                        <p class="text-[12px] md:text-[20px] font-semibold text-[#1D1717] mt-3">
+                            {{ card.title }}
+                        </p>
+                    </div>
+                    <div class=" md:hidden h-full flex flex-col justify-around ">
+                        <p class="text-[12px] font-semibold text-[#1D1717] my-3">
+                            {{ card.title }}
+                        </p>
+                        <div class="flex justify-between items-center mt-auto">
+                            <div class="border-l-4 border-black">
+                                <p class="ml-2 text-[14px] font-bold text-[#002E40]">
+                                    {{ card.author }}
+                                </p>
+                            </div>
+                            <p class="text-[12px] text-[#002E40]">{{ card.date }}</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
         </div>
-    </div>
     </section>
 </template>
 
--- src/component/Video.vue
+++ src/component/Video.vue
@@ -52,8 +52,9 @@ export default {
 </script>
 
 <template>
-  <section class="py-[30px] md:py-[40px] ">
+  <section class="py-[30px] md:py-[40px]">
     <div class="container lg:max-w-[1200px] mx-auto">
+      <!-- Header -->
       <div class="grid grid-cols-2 md:grid-cols-2 px-[24px] md:px-0">
         <div class="flex gap-1 flex-wrap">
           <p class="text-[18px] leading-[21.6px] md:text-[40px] md:leading-[48px] font-semibold font-bricolage text-black">Video</p>
@@ -74,46 +75,40 @@ export default {
         <div class="border-[2px] w-[26px] md:w-[32px] border-black mt-2"></div>
       </div>
 
+      <!-- Video Section -->
       <div class="grid grid-cols-1 md:grid-cols-2 gap-6 pt-[24px] md:pt-[20px]">
         <!-- Multiple Videos (Desktop View) -->
-        <div class="space-y-6 order-2 md:order-1 px-[24px] md:px-0">
-          <div v-for="(item, index) in hero8VideoData" :key="index" class="flex gap-4 items-center justify-center">
+        <div class="space-mb-6 order-2 md:order-1 px-[24px] md:px-0">
+          <div v-for="(item, index) in hero8VideoData" :key="index" class="Element flex gap-4 items-center justify-center" :class="{ 'no-border': index === hero8VideoData.length - 1 }">
             <div class="relative w-[50%] md:w-[200px]">
-              <video :src="item.video_berita_hero8.url" controls class="w-[full] h-[auto] mb-0"></video>
+              <video :src="item.video_berita_hero8.url" controls class="w-full h-auto mb-0"></video>
             </div>
             <div class="w-[50%] md:w-[342px] md:h-[160px]">
               <h2 class="text-[14px] leading-[20px] md:text-[20px] md:leading-[24px] font-bold text-black font-bricolage">{{ item.judul_berita_hero8_video }}</h2>
               <div class="hidden md:block">
-                <p id="readMoreText"
-                class="mt-[12px] text-[14px] leading-[22px] md:text-[16px] md:leading-[22px] font-bricolage overflow-hidden transition-all duration-300 ease-in-out"
+                <p id="readMoreText" class="mt-[12px] text-[14px] leading-[22px] md:text-[16px] md:leading-[22px] font-bricolage overflow-hidden transition-all duration-300 ease-in-out"
                 :class="readMoreText ? 'max-h-full' : 'max-h-[1.5rem]'">{{ item.title_berita_tengah_hero8 }}</p>
-                <button id="readMoreBtn" class="text-blue-500 font-bricolage font-semibold hover:underline focus:outline-none "
-                @click="toggleReadMore">
-                {{ readMoreText ? 'Read Less' : 'Read More' }}
-              </button>
-            </div>
+                <button id="readMoreBtn" class="text-blue-500 font-bricolage font-semibold hover:underline focus:outline-none"
+                @click="toggleReadMore">{{ readMoreText ? 'Read Less' : 'Read More' }}</button>
+              </div>
               <div class="flex justify-between text-sm text-gray-500 font-bricolage mt-[12px]">
                 <span>{{ item.title_berita_bawah_kanan_hero8 }}</span>
                 <span>{{ item.title_berita_bawah_kiri_hero8 }}</span>
               </div>
             </div>
           </div>
-          <div class="border-t-2 border-gray-500 my-2"></div>
         </div>
 
         <!-- Main Video (Desktop View) -->
-        <div class="relative order-1 ">
+        <div class="relative order-1">
           <video :src="hero8video.video_hero8.url" controls class="w-full h-auto"></video>
           <div class="mt-4 px-[24px] md:px-0">
             <h2 class="text-[20px] leading-[24px] font-bricolage font-bold text-black">{{ hero8video.judul_video8 }}</h2>
-            <p id="readMoreText"
-              class="mt-[12px] text-[14px] leading-[22px] md:text-[16px] md:leading-[22px] font-bricolage overflow-hidden transition-all duration-300 ease-in-out"
+            <p id="readMoreText" class="mt-[12px] text-[14px] leading-[22px] md:text-[16px] md:leading-[22px] font-bricolage overflow-hidden transition-all duration-300 ease-in-out"
               :class="readMoreText ? 'max-h-full' : 'max-h-[1.5rem]'">{{ hero8video.deskripsi_video_8 }}</p>
-            <button id="readMoreBtn" class="text-blue-500 font-bricolage font-semibold hover:underline focus:outline-none "
-              @click="toggleReadMore">
-              {{ readMoreText ? 'Read Less' : 'Read More' }}
-            </button>
-            <div class="hidden md:block"> 
+            <button id="readMoreBtn" class="text-blue-500 font-bricolage font-semibold hover:underline focus:outline-none"
+              @click="toggleReadMore">{{ readMoreText ? 'Read Less' : 'Read More' }}</button>
+            <div class="hidden md:block">
               <div class="flex justify-between text-sm text-gray-500 font-bricolage mt-[12px]">
                 <span>{{ hero8video.text_kanan_video8 }}</span>
                 <span>{{ hero8video.text_kiri_video8 }}</span>
@@ -127,5 +122,13 @@ export default {
 </template>
 
 <style scoped>
-/* Custom styles jika diperlukan */
+.Element {
+    border-bottom: 2px solid #0000004D;
+    padding-bottom: 10px; /* Tambahkan jarak bawah */
+    padding-top: 10px;    /* Tambahkan jarak atas */
+}
+/* Gaya untuk item terakhir (tanpa border) */
+.Element.no-border {
+    border-bottom: none;
+}
 </style>
